d908e8c998c4a46f09cdcbac903a8690afffc23e
Donghak Kim <me@fodo.dev>
2024-11-29 15:37:11 +0900

    Update deploy_lambda.sh

--- deploy_lambda.sh
+++ deploy_lambda.sh
@@ -1,4 +1,4 @@
-aws s3 cp https://text-to-image-bucket-lcl50j80.s3.us-west-2.amazonaws.com/SEdufit-Build-Project/function.zip /tmp/function.zip
+aws s3 cp https://text-to-image-bucket-hmwd1zvx.s3.us-west-2.amazonaws.com/SEdufit-Build-Project/function.zip /tmp/function.zip
 
 aws lambda update-function-code \
   --function-name callback-lambda \
