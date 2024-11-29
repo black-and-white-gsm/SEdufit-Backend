b8c12fa5005a3bc9c58568b0664b29063a49acf2
Jueuunn7 <wndmsrla1234@gmail.com>
2024-11-29 14:11:58 +0900

    Revert "Add lambda"
    This reverts commit 3b72c6619dc7c63739b90a3e65a9a31f8b5161ba.

--- main.py
+++ main.py
@@ -1,39 +1,16 @@
-import os
-import requests
-from langchain_community.chat_models import ChatPerplexity
-from langchain_core.prompts import ChatPromptTemplate
+# 샘플 Python 스크립트입니다.
+
+# ⌃F5을(를) 눌러 실행하거나 내 코드로 바꿉니다.
+# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 ⇧ 두 번을(를) 누릅니다.
 
 
-api_key=os.environ.get('API_KEY')
-model='llama-3.1-sonar-small-128k-online'
-chat = ChatPerplexity(pplx_api_key=api_key, model=model)
+def print_hi(name):
+    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
+    print(f'Hi, {name}')  # 중단점을 전환하려면 F9을(를) 누릅니다.
 
 
-def perplexity_chat(message):
-    prompt = ChatPromptTemplate.from_messages([
-        ('system', os.environ.get('SYSTEM')),
-        ('human', message)
-    ])
+# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
-    chain = prompt | chat
-    response = chain.invoke({'question': message})
-    return response.content
-
-
-def lambda_handler(event, context):
-    print(event)
-    text_input = event['text_input']
-    callback_url = event['callback_url']
-
-    result = perplexity_chat(text_input)
-    print(result)
-
-    requests.post(
-        callback_url,
-        json={
-            'version': '2.0',
-            'useCallback': False,
-            'template': {'outputs': [{'simpleText': {'text': f'{result}'}}]}
-        }
-    )
-    return True
+# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
