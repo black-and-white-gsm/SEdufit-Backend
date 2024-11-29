ec2746e3e159c1c2edc112e4cd7f970ca41646e0
Donghak Kim <me@fodo.dev>
2024-11-29 15:22:02 +0900

    Create deploy_lambda.sh

---
+++ deploy_lambda.sh
@@ -0,0 +1,5 @@
+aws s3 cp https://text-to-image-bucket-lcl50j80.s3.us-west-2.amazonaws.com/SEdufit-Build-Project/function.zip /tmp/function.zip
+
+aws lambda update-function-code \
+  --function-name callback-lambda \
+  --zip-file fileb:///function.zip
