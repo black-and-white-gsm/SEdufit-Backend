84b5f367302dcf245d5a41d70fff9ca3a85f3e8b
Jueuunn7 <wndmsrla1234@gmail.com>
2024-11-29 14:48:46 +0900

    Edit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # SEdufit-Backend
+
+## env
+```
+export API_KEY=<API KEY>
+export SYSTEM=<TEMPLATE>
+```
+
+## venv
+```
+.venv/bin/avtivate
+pip3 install -r requirements.txt
+```
+
+## run
+```
+python3 test.py
+```
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import requests
+import logging
 from langchain_community.chat_models import ChatPerplexity
 from langchain_core.prompts import ChatPromptTemplate
 
 
+logger = logging.getLogger()
 api_key=os.environ.get('API_KEY')
 model='llama-3.1-sonar-small-128k-online'
 chat = ChatPerplexity(pplx_api_key=api_key, model=model)
-
 
 def perplexity_chat(message):
     prompt = ChatPromptTemplate.from_messages([
@@ -15,25 +16,30 @@ def perplexity_chat(message):
         ('human', message)
     ])
 
-    chain = prompt | chat
+    try:
+        chain = prompt | chat
+    except Exception as e:
+        logger.error(f'perplexity 오류: {e}')
+
     response = chain.invoke({'question': message})
     return response.content
 
 
 def lambda_handler(event, context):
-    print(event)
     text_input = event['text_input']
     callback_url = event['callback_url']
 
     result = perplexity_chat(text_input)
-    print(result)
+    try:
+        requests.post(
+            callback_url,
+            json={
+                'version': '2.0',
+                'useCallback': False,
+                'template': {'outputs': [{'simpleText': {'text': f'{result}'}}]}
+            }
+        )
+    except Exception as e:
+        logger.error(f'콜백 전송 실패: {e}')
 
-    requests.post(
-        callback_url,
-        json={
-            'version': '2.0',
-            'useCallback': False,
-            'template': {'outputs': [{'simpleText': {'text': f'{result}'}}]}
-        }
-    )
     return True
